e33c8f5041ca1a99eb53946769e1a08cd61904a9
Santhoshlolam <119613155+Santhoshlolam@users.noreply.github.com>
2023-03-15 05:55:42 -0700

    Time: 2 ms (64.90%), Space: 40.6 MB (30.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,75 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==1)
+        return false;
+       else {
+        Stack<Character> ok=new Stack<Character>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{' )
+              ok.push(s.charAt(i));
+            else if(s.charAt(i)==')')
+            {
+                if( ok.size()!=0  && ok.peek()=='(')
+                ok.pop();
+                else
+                    return false;
+            }
+            else if(s.charAt(i)=='}')
+            {
+                if(ok.size()!=0 && ok.peek()=='{')
+                ok.pop();
+                else return false;
+            }
+            else if(s.charAt(i)==']')
+            {
+                if(ok.size()!=0 && ok.peek()=='[')
+                ok.pop();
+                else
+                    return false;
+            }
+            
+        }
+      //  System.out.println(ok);
+        if(ok.size()==0)
+        return true;
+        else
+        return false;
+    }  
+ /*           Stack<Character> ok = new Stack<>();
+
+        for(char ch: s.toCharArray())
+        {
+            switch (ch)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return stack.isEmpty();  */
+
+    
+    }
+}
