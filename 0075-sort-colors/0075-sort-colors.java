3ea1d9b96ee052494071757d0aa509650572494a
Santhoshlolam <119613155+Santhoshlolam@users.noreply.github.com>
2023-07-29 14:17:36 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (85.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
      
-      for(int i=0;i<nums.length;i++)
+     /* for(int i=0;i<nums.length;i++)
       {
         int flag=0;
           for(int j=0;j<nums.length-1-i;j++)
@@ -17,7 +17,34 @@ class Solution {
           if(flag==0)
               break;
         
-      }  
+      }  */
+        
+       int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+                mid++;
+            else
+            {
+                if(nums[mid]==2)
+                {
+                    int temp=nums[high];
+                    nums[high]=nums[mid];
+                    nums[mid]=temp;
+                    high--;
+                }
+            }
+        }
         
         
     }
